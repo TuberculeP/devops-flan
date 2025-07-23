f2b15a08003c86328f83ab5ac1fb1c20eed6d074
Félix Laviéville <lavieville.felix@gmail.com>
2025-07-23 10:24:44 +0200

    feat: adding user count on main

--- webapp/src/views/Main.vue
+++ webapp/src/views/Main.vue
@@ -1,7 +1,10 @@
+<script setup lang="ts">
+import TotalUsersComponent from "../components/TotalUsersComponent.vue";
+</script>
+
 <template>
   <div>
     <h1>Accueil</h1>
+    <TotalUsersComponent />
   </div>
 </template>
-
-<script setup lang="ts"></script>
