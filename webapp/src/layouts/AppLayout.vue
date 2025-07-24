7674828ed4796df0ff17250ba1772b41c404826e
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2025-07-24 15:30:40 +0200

    feat : nav & style

--- webapp/src/layouts/AppLayout.vue
+++ webapp/src/layouts/AppLayout.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>
-    <div v-if="isAuthenticated">
-      Bienvenue, {{ user?.firstName }}
-      <button @click="disconnect">Déconnexion</button>
+  <section v-if="isAuthenticated">
+    <div class="nav">
+      <router-link
+        to="/"
+        class="product-link"
+        style="text-decoration: none; color: black"
+      >
+        <h2>Devops-Flan</h2>
+      </router-link>
+      <div id="router">
+        <router-link to="/" class="product-link">
+          <button class="bouton route">Home</button>
+        </router-link>
+
+        <router-link to="/products" class="product-link">
+          <button class="bouton route">Produits</button>
+        </router-link>
+
+        <router-link to="/dashboard" class="product-link">
+          <button class="bouton route">Dashboard</button>
+        </router-link>
+        <button class="bouton disconnect" @click="disconnect">
+          Déconnexion
+        </button>
+      </div>
     </div>
+  </section>
+  <div>
     <slot />
   </div>
 </template>
@@ -26,3 +49,58 @@ async function disconnect() {
   }
 }
 </script>
+
+<style scoped>
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 2em;
+  align-items: center;
+  text-align: center;
+  background-color: rgb(215, 237, 239);
+  color: black;
+  margin-bottom: 15px;
+
+  height: 80px;
+  width: 100vw;
+
+  #router {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+  }
+}
+.bouton {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: rgb(215, 237, 239);
+  color: #000000;
+  text-align: center;
+  text-decoration: none;
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition:
+    background-color 0.3s ease,
+    transform 0.2s ease;
+}
+
+.route {
+  background-color: rgb(215, 237, 239);
+  &:hover {
+    background-color: rgb(185, 215, 211);
+    transform: scale(1.05);
+  }
+}
+
+.disconnect {
+  background-color: rgb(166, 218, 222);
+  &:hover {
+    background-color: rgb(131, 194, 199);
+    transform: scale(1.05);
+  }
+}
+</style>
